5f8f3478235a55a40cc568ec7cc8522efe026bbc
onursolmaz <onursolmaz@gmail.com>
2018-10-06 11:59:36 +0300

    Minor

--- mnemonic.py
+++ mnemonic.py
@@ -1,50 +1,54 @@
-from hexbytes import HexBytes
 import copy
-
+from hexbytes import HexBytes
 
 seed_words = open('mnemonic_words.txt').read().split('\n')
 seed_words.remove('')
 
 seed_words_idx = {}
-# seed_words_idx_inv = {}
 
 for n, w in enumerate(seed_words):
     seed_words_idx[w] = n
 
-# print(HexBytes(test_addr))
-# print(int(test_addr[2:], 16))
-
-
 def number_to_addr(number):
     return "0x%0.40x"%number
 
-def get_indices(number, base):
-    n_idx = 0
-    number_ = copy.copy(number)
-    indices = []
+def get_different_base_indices(x, base):
+    'Get a list of indices of digits of x in a different base'
 
-    while base**n_idx < number:
-        n_idx += 1
+    if not isinstance(x, int):
+        raise Exception('Input number must be of type int')
+    if x < 0:
+        raise Exception('Negative numbers not supported')
+    elif x == 0:
+        return [0]
 
-    n_idx -= 1
-    while n_idx >= 0:
-        current_idx = 0
-        digit_value = base**n_idx
-        while number_ - digit_value >= 0:
-            # print(number_)
-            number_ -= digit_value
-            current_idx += 1
+    digits = []
 
-        indices.append(current_idx)
-        n_idx -= 1
-    return list(reversed(indices))
+    while x > 0:
+        digits.append(x % base)
+        x = x // base
 
+    digits.reverse()
 
-# test = sum([i*2048**n for n,i in enumerate(indices)])
-# print(test == number)
+    return digits
 
-# def mnemonic_to_addr(mnemonic):
-#     return addr
+# def get_different_base_indices(number, base):
+#     'Alternative implementation'
+#     n_idx = 0
+#     number_ = copy.copy(number)
+#     indices = []
+#     while base**(n_idx+1) < number:
+#         n_idx += 1
+#     while n_idx >= 0:
+#         current_idx = 0
+#         digit_value = base**n_idx
+#         while number_ - digit_value >= 0:
+#             number_ -= digit_value
+#             current_idx += 1
+#         indices.append(current_idx)
+#         n_idx -= 1
+#     return indices
+
 
 class Mnemonic:
     def __init__(self):
@@ -54,7 +58,7 @@ class Mnemonic:
         new_obj.addr = addr
 
         number = int(addr[2:], 16)
-        indices = get_indices(number, 2048)
+        indices = get_different_base_indices(number, 2048)
         new_obj.list_ = [seed_words[i] for i in indices]
         return new_obj
 
@@ -66,7 +70,7 @@ class Mnemonic:
         new_obj = Mnemonic()
         new_obj.list_ = list_
         indices = [seed_words_idx[word] for word in list_]
-        number = sum([i*2048**n for n,i in enumerate(indices)])
+        number = sum([i*2048**n for n,i in enumerate(reversed(indices))])
         new_obj.addr = number_to_addr(number)
         return new_obj
 
@@ -84,11 +88,16 @@ class Mnemonic:
     def get_addr(self):
         return self.addr
 
-# test_addr = '0xa62142888aba8370742be823c1782d17a0389da1'
-# mnemonic = Mnemonic.from_addr(test_addr)
-# print(mnemonic)
-# print(mnemonic.get_trunc_str())
+if __name__ == '__main__':
+    test_addr = '0xa62142888aba8370742be823c1782d17a0389da1'
 
-# mnemonic2 = Mnemonic.from_str(mnemonic.get_str())
-# print(mnemonic2.get_addr())
+    mnemonic = Mnemonic.from_addr(test_addr)
+    print(mnemonic)
+    print(mnemonic.get_trunc_str())
 
+    mnemonic2 = Mnemonic.from_str(mnemonic.get_str())
+    print(mnemonic2.get_addr())
+    print(mnemonic2.get_addr()==test_addr)
+
+    # number = int(test_addr[2:], 16)
+    # print(get_different_base_indices(number, 2048))
